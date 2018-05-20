23cbfc59763a086233224114d903191a16b4cbd0
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-05-20 15:00:17 +0900

    Add files via upload

---
+++ admin/admin_login.php
@@ -0,0 +1,10 @@
+<?php
+  header('Content-Type: text/html; charset=utf-8');
+
+    echo "<h1>&#60;관리자 페이지로 접속합니다&#62;</h1>";
+    echo "<p>비밀번호를 입력하세요.</p>";
+    echo "<form action='./admin_login_action.php' method='post' style='display : inline-flex'>";
+    echo "<label id='password_tag'> Password </label> <input type='password' id='password_input' name='isPassword'>";
+    echo "<input type='submit' id='submit_button' value='입력받기'> </b></form>";
+
+?>
---
+++ admin/admin_login_action.php
@@ -0,0 +1,16 @@
+<?php
+  header('Content-Type: text/html; charset=utf-8');
+  include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+  
+  // set password
+  $pass = "123456";
+  
+  // configuration password
+  if($_POST["isPassword"]==$pass){
+      echo("<script>location.replace('./menu.html');</script>"); 
+    }
+    else{
+      echo("<script>alert('비밀번호가 틀렸습니다');</script>");
+      echo("<script>location.replace('/index.html');</script>"); 
+    }
+?>
