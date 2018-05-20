b2a12f2c82f2320d7a4afc7428a305db4f85bafa
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-05-20 18:16:44 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,11 @@
 <?php
-// utf-8 setting
-putenv("NLS_LANG=KOREAN_KOREA.KO16KSC5601");
-
 // Create connection to Oracle
 
 $server = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=mydbinstance.chbkz2j61mlx.ap-northeast-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORCL)))";
 $username = "YongJun";
 $password = "123456789";
 
-$conn = oci_connect($username, $password,$server);
+$conn = oci_connect($username, $password,$server,'AL32UTF8');
 if (!$conn) {
    $m = oci_error();
    echo $m['message'], "\n";
