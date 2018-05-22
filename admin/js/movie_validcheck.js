d2880a15a8650567e204352885d3d37bb6110067
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-05-22 12:53:48 +0900

    Update movie_validcheck.js

--- admin/js/movie_validcheck.js
+++ admin/js/movie_validcheck.js
@@ -2,12 +2,32 @@ function validcheck(){
   
   form = document.insertform;
   
-  if(form.song_name.value==""){
-    alert("곡 제목은 필수입력사항입니다");
+  if(form.mv_nm.value==""){
+    alert("영화 제목을 입력해주세요");
     return false;
   }
-  else if(form.song_artist.value==""){
-    alert("아티스트는 필수입력사항입니다");
+  else if(form.dir_nm.value==""){
+    alert("감독을 입력해주세요");
+    return false;
+  }
+  else if(form.mv_int.value==""){
+    alert("영화 소개를 입력해주세요");
+    return false;
+  }
+  else if(form.atr_nm1.value==""){
+    alert("배우를 1명 이상 입력해주세요");
+    return false;
+  }
+  else if(form.scr_time.value==""){
+    alert("상영 시간을 입력해주세요");
+    return false;
+  }
+  else if(form.genre.value==""){
+    alert("영화 장르를 입력해주세요");
+    return false;
+  }
+  else if(form.rate.value==""){
+    alert("관람 등급을 입력해주세요");
     return false;
   }
   else{
