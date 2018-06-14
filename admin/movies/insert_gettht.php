4074d5597de8c559fc55380e1ed4b69e122481c1
root <root@ip-172-31-16-116.ap-northeast-2.compute.internal>
2018-06-14 06:05:53 +0000

    0614 insert_gettht.php

--- admin/movies/insert_gettht.php
+++ admin/movies/insert_gettht.php
@@ -1,18 +1,15 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
    // Parse SQL
-    $confirm = "SELECT THT.THT_NM FROM LOC,THT WHERE LOC.LOC_NUM=THT.LOC_NUM AND LOC.LOC_NM = '왕십리'";
+    $confirm = "SELECT THT.THT_NM FROM LOC,THT WHERE LOC.LOC_NUM=THT.LOC_NUM AND LOC.LOC_NM = '$_POST[loc]'";
     $result = oci_parse($conn,$confirm);
     oci_execute($result);
-        	
-    echo "<script type='text/javascript'>";
-
+     
     while($row=oci_fetch_row($result))
     {
-	echo "$('#tht_nm').html('<option value=".$row[0]."> $row[0] </option>');";	
-
+	$data[] = array("tht_nm"=>$row[0]);	
     }
 
-    echo "</script>";
-
+    echo json_encode($data,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+	   
 ?>
