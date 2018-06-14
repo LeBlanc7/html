a5adbc96d6a2997cdef5ae6040ecf978150ba443
root <root@ip-172-31-16-116.ap-northeast-2.compute.internal>
2018-06-14 14:28:53 +0000

    0614 ticket loc_list

---
+++ ticket/get_loc_list.php
@@ -0,0 +1,21 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+	// Parse SQL
+	$confirm = "SELECT DISTINCT LOC_NUM FROM SCR_SCH WHERE MV_NUM = '$_POST[mv_num]'";
+	$result = oci_parse($conn,$confirm);
+	oci_execute($result);
+	
+	while($row=oci_fetch_row($result))
+	{
+		$loc_num = $row[0];
+		$query = "SELECT LOC_NUM,LOC_NM FROM LOC WHERE LOC_NUM='$loc_num'";
+		$temp = oci_parse($conn,$query);
+		oci_execute($temp);
+		$temp_row = oci_fetch_row($temp);
+			
+		$data[] = array("loc_num"=>$temp_row[0],"loc_nm"=>$temp_row[1]);
+	}
+	
+	echo json_encode($data,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+
+?>
