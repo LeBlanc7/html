1a104ffe00e2536d7160b4c68046cc648cfaed8c
root <root@ip-172-31-16-116.ap-northeast-2.compute.internal>
2018-06-14 15:49:14 +0000

    0615 get_datelist

---
+++ ticket/get_date_list.php
@@ -0,0 +1,14 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+	// Parse SQL
+	$confirm = "SELECT DISTINCT SCH_DAY FROM SCR_SCH WHERE MV_NUM = '$_POST[mv_num]' AND LOC_NUM='$_POST[loc_num]'";
+	$result = oci_parse($conn,$confirm);
+	oci_execute($result);
+	
+	while($row=oci_fetch_row($result))
+	{
+		$data[] = array("sch_day"=>$row[0]);
+	}
+	
+	echo json_encode($data,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+?>
