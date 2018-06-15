fcd61c710040c320401e9d30de86fe7949bbcd4c
root <root@ip-172-31-16-116.ap-northeast-2.compute.internal>
2018-06-15 13:35:50 +0000

    select_seat 20%

--- ticket/ticket_seat_avabinfo.php
+++ ticket/ticket_seat_avabinfo.php
@@ -17,28 +17,56 @@
 	$confirm = "SELECT SEAT_NUM,SEAT_ROW,SEAT_COL FROM SEAT WHERE THT_NUM='$tht_num' AND LOC_NUM='$loc_num'";
 	$result = oci_parse($conn,$confirm);
 	oci_execute($result);
-	
-	if($temp == 240) 
+		
+	if($seatN == 240) 
 	{
-		int $i = 1;
+		$i = 1;
+		$flag = 0;
 
 		while($row = oci_fetch_row($result))
 		{
+			$seat_num = $row[0];
 			$seat_row = $row[1];
-			$seat_col = $row[2];
-			$seat_nm = $seat_row + $seat_col;
-			
-			if($i<=20)
+			$seat_col = $row[2];			
+			$seat_nm = $seat_row.$seat_col;
+	
+			$confirm = "SELECT COUNT(*) AS COUNT FROM SEAT_AVAB WHERE THT_NUM='$tht_num' AND LOC_NUM='$loc_num' AND SCH_NUM='$_GET[sch_num]' AND SEAT_NUM='$seat_num'";
+        		$result1 = oci_parse($conn,$confirm);
+        		oci_execute($result1);
+			$temp = oci_fetch_row($result1);
+			$valid = $temp[0];
+
+			if($valid>0)
 			{
 
 			}
-			else if(20< $i && $i <=40)
+			else
 			{
+				if($i<=20)
+				{
+					if($flag==0)
+					{
+						echo "<div id='".$seat_row."'>";
+						echo "<input type='checkbox' name='check[]' value='".$seat_nm."'>";
+						$flag = 1;
+					}
+					else
+					{
+						echo "<input type='checkbox' name='check[]' value='".$seat_nm."'>";
+					}
+					if($i==20)
+					{
+						echo "</div>";
+						$flag = 0;
+					}
+				}
+				else if(20< $i && $i <=40)
+				{	
 
+				}	
+			
+			
 			}
-			
-			
-
 			$i++;					
 
 		}
@@ -47,5 +75,5 @@
 	{
 
 	}	
-	
+		
 ?>
